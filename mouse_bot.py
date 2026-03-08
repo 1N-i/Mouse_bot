88018207042ec91cc696194a70b75ee5fcaa6d96
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-07 22:23:23 -0300

    Add files via upload

---
+++ mouse_bot.py
@@ -0,0 +1,25 @@
+import pyautogui
+from random import randint
+from time import sleep
+from playsound3 import playsound
+import os
+
+pyautogui.FAILSAFE = True
+path = os.path.dirname(os.path.abspath(__file__))
+sound = os.path.join(path, "alarm.mp3")
+clicks = input("Clicks (Y/N)? ").lower()
+
+try:
+    while True:
+        #Numbers can switch depending on what you want to do
+        x = randint(200, 1200)
+        y = randint(200, 650)
+        if clicks == "y":
+            pyautogui.mouseDown()
+            pyautogui.mouseUp()
+        pyautogui.moveTo(x, y, 0.5)
+        sleep(2)
+
+except pyautogui.FailSafeException:
+    playsound(sound)
+    print("The bot has stopped!")
